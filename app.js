152a51cc971ab03dfd932cd1c9996b20fcbe07ac
Hoamp <setiawanthomas43@gmail.com>
2022-11-12 19:29:41 +0700

    menghapus comentar

--- app.js
+++ app.js
@@ -6,21 +6,21 @@ console.log(process.argv);
 
 
 
-// // menggunakan require untuk bisa menggunakan function di contacts.js
-// // const {tulisPertanyaan, simpanContact} = require('./contacts.js');
-// const contacts = require('./contacts.js');
+// menggunakan require untuk bisa menggunakan function di contacts.js
+// const {tulisPertanyaan, simpanContact} = require('./contacts.js');
+const contacts = require('./contacts.js');
 
-// // main function
-// const main = async () => {
-//     // menangkap pertanyaan
-//     const nama = await contacts.tulisPertanyaan("Masukkan nama anda : ");
-//     const email = await contacts.tulisPertanyaan("Masukkan email anda : ");
-//     const noHp = await contacts.tulisPertanyaan("Masukkan noHp anda : ");
+// main function
+const main = async () => {
+    // menangkap pertanyaan
+    const nama = await contacts.tulisPertanyaan("Masukkan nama anda : ");
+    const email = await contacts.tulisPertanyaan("Masukkan email anda : ");
+    const noHp = await contacts.tulisPertanyaan("Masukkan noHp anda : ");
 
-//     // simpan inputan user
-//     contacts.simpanContact(nama, email, noHp);
+    // simpan inputan user
+    contacts.simpanContact(nama, email, noHp);
     
-// };
+};
 
-// // menjalankan main function
-// main();
+// menjalankan main function
+main();
